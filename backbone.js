991bd88504ac376f77a4142aa73e4a9454d0d1ac
Julian Gonggrijp <dev@juliangonggrijp.com>
2023-07-24 01:22:27 +0200

    Move _.extend(Backbone, Events) to modules/index-default.js

--- backbone.js
+++ backbone.js
@@ -50,10 +50,6 @@
     return this;
   };
 
-  // Allow the `Backbone` object to serve as a global event bus, for folks who
-  // want global "pubsub" in a convenient place.
-  _.extend(Backbone, Events);
-
   // Backbone.Model
   // --------------
 
---
+++ modules/index-default.js
@@ -0,0 +1,4 @@
+
+  // Allow the `Backbone` object to serve as a global event bus, for folks who
+  // want global "pubsub" in a convenient place.
+  _.extend(Backbone, Events);
