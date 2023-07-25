cf04ad93ee66a2f1134d15c7781ef948644a7c48
Julian Gonggrijp <dev@juliangonggrijp.com>
2023-07-25 13:56:55 +0200

    squash! Rebuild the UMD bundle using Rollup (WIP)
    'use strict' shouldn't be there.

--- rollup.config.js
+++ rollup.config.js
@@ -19,6 +19,7 @@ export default [{
             'underscore/modules/extend.js': '_.extend',
         },
         freeze: false,
+        strict: false,
         sourcemap: true,
         sourcemapExcludeSources: true,
     },
