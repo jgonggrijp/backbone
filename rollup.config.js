0558f9dfafdd4b5f86fefca8d8b00d4eb68a6f43
Julian Gonggrijp <dev@juliangonggrijp.com>
2023-07-25 13:29:52 +0200

    squash! Rebuild the UMD bundle using Rollup (WIP)
    This should bring the resulting UMD bundle much closer to the original
    situation.

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,24 @@
+import resolve from '@rollup/plugin-node-resolve';
+
 export default [{
     input: 'modules/index-default.js',
     treeshake: false,
+    external: [/node_modules/],
+    plugins: [
+        resolve(),
+    ],
     output: {
         file: 'backbone.js',
         exports: 'default',
         format: 'umd',
         name: 'Backbone',
-        amd: {
-            id: 'backbone,'
-        },
         noConflict: true,
+        globals: {
+            underscore: '_',
+            jquery: 'jQuery || Zepto || ender || $',
+            'underscore/modules/extend.js': '_.extend',
+        },
+        freeze: false,
         sourcemap: true,
         sourcemapExcludeSources: true,
     },
